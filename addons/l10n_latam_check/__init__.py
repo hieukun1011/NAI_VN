212b8c971ff77e34a93dea139295f6c4d599780e
hieu.duong <hieu.duong@icomm-group.com>
2024-06-13 12:59:54 +0700

    Update crm

--- addons/l10n_latam_check/__init__.py
+++ addons/l10n_latam_check/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 from . import models
 from . import wizards
-=======
-from . import models
-from . import wizards
->>>>>>> 1edc56903ab79c74a4459e1792ad6ae33a23a2ad
--- addons/web/static/lib/fullcalendar/google-calendar/main.js
+++ addons/web/static/lib/fullcalendar/google-calendar/main.js
@@ -10,7 +10,6 @@ Docs & License: https://fullcalendar.io/
     (global = global || self, factory(global.FullCalendarGoogleCalendar = {}, global.FullCalendar));
 }(this, function (exports, core) { 'use strict';
 
-<<<<<<< HEAD
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
     Licensed under the Apache License, Version 2.0 (the "License"); you may not use
@@ -35,32 +34,6 @@ Docs & License: https://fullcalendar.io/
             return t;
         };
         return __assign.apply(this, arguments);
-=======
-    /*! *****************************************************************************
-    Copyright (c) Microsoft Corporation. All rights reserved.
-    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-    this file except in compliance with the License. You may obtain a copy of the
-    License at http://www.apache.org/licenses/LICENSE-2.0
-
-    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-    MERCHANTABLITY OR NON-INFRINGEMENT.
-
-    See the Apache Version 2.0 License for specific language governing permissions
-    and limitations under the License.
-    ***************************************************************************** */
-
-    var __assign = function() {
-        __assign = Object.assign || function __assign(t) {
-            for (var s, i = 1, n = arguments.length; i < n; i++) {
-                s = arguments[i];
-                for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-            }
-            return t;
-        };
-        return __assign.apply(this, arguments);
->>>>>>> 1edc56903ab79c74a4459e1792ad6ae33a23a2ad
     };
 
     // TODO: expose somehow
