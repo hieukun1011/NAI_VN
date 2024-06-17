06de6d86b29d387648e504d789e53bc8df6ab38d
hieu.duong <hieu.duong@icomm-group.com>
2024-06-17 18:54:38 +0700

    1. check trùng khách hàng 2.  thiết lập và linking điểm với loyaly card

--- addons_crm/pontusinc_loyalty/models/res_company.py
+++ addons_crm/pontusinc_loyalty/models/res_company.py
@@ -4,9 +4,9 @@ from odoo import fields, models, api, _
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    # auto_active = fields.Selection([('have_orders', 'Generate orders after opening the card'),
-    #                                 ('auto_activate_card', 'Automatically activate the card')],
-    #                                default='auto_activate_card', required=True)
+    x_auto_active = fields.Selection([('have_orders', 'Generate orders after opening the card'),
+                                    ('auto_activate_card', 'Automatically activate the card')],
+                                   default='auto_activate_card', required=True)
     have_orders = fields.Boolean('Generate orders after opening the card')
     auto_activate_card = fields.Boolean('Automatically activate the card')
     proviso = fields.Selection([('or', 'OR'),
