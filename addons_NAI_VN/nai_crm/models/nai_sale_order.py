7f3854cdfbcc1cc7d035a76846ef3c2d3483fe52
hieu.duong <hieu.duong@icomm-group.com>
2025-02-19 16:33:03 +0700

    update

--- addons_NAI_VN/nai_crm/models/nai_sale_order.py
+++ addons_NAI_VN/nai_crm/models/nai_sale_order.py
@@ -4,13 +4,17 @@ from odoo import fields, models, _
 class NAISaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    template_report_id = fields.One2many('popup.select.fields.report', 'sale_order_id')
+
+
     def action_print_so(self):
         self.ensure_one()
-        view_tree_id = self.env.ref('nai_crm.popup_select_fields_report_tree_view').id
         view_form_id = self.env.ref('nai_crm.popup_select_fields_report_form_view').id
         context = {
             'default_sale_order_id': self.id,
+            'create': False
         }
+        domain = []
         if self.order_line:
             if len(self.order_line) > 1:
                 for rec in self.order_line:
@@ -23,6 +27,8 @@ class NAISaleOrder(models.Model):
             'type': 'ir.actions.act_window',
             'views': [(view_form_id, 'form')],
             'res_model': 'popup.select.fields.report',
+            'view_mode': 'form',
+            'res_id': self.template_report_id[-1].id if self.template_report_id else False,
             'target': 'new',
             'context': context
         }
